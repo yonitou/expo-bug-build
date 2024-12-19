77238eae20bd9348f5ce1360a6e8d427c459c407
SanjayDevTech <sureshsanjay805@gmail.com>
2024-12-19 19:45:23 +0530

    added bottom bar

--- App.js
+++ App.js
@@ -1,11 +1,110 @@
-import { StyleSheet, Text, View } from 'react-native';
 import { SystemBars } from 'react-native-edge-to-edge';
+import { PaperProvider, Surface, Text, Appbar, BottomNavigation } from 'react-native-paper';
+import { CommonActions } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { NavigationContainer } from '@react-navigation/native';
+import { View, StyleSheet } from 'react-native';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
+    <NavigationContainer>
+      <PaperProvider>
+        <Surface style={{
+          flex: 1,
+        }}>
+          <Appbar.Header elevated>
+            <Appbar.Content title="Edge To Edge Test App" />
+          </Appbar.Header>
+          <SystemBars style="auto" />
+          <Tab.Navigator
+            screenOptions={{
+              headerShown: false,
+            }}
+            initialRouteName='Home'
+            tabBar={({ navigation, state, descriptors, insets }) => (
+              <BottomNavigation.Bar
+                navigationState={state}
+                safeAreaInsets={insets}
+                onTabPress={({ route, preventDefault }) => {
+                  const event = navigation.emit({
+                    type: 'tabPress',
+                    target: route.key,
+                    canPreventDefault: true,
+                  });
+
+                  if (event.defaultPrevented) {
+                    preventDefault();
+                  } else {
+                    navigation.dispatch({
+                      ...CommonActions.navigate(route.name, route.params),
+                      target: state.key,
+                    });
+                  }
+                }}
+                renderIcon={({ route, focused, color }) => {
+                  const { options } = descriptors[route.key];
+                  if (options.tabBarIcon) {
+                    return options.tabBarIcon({ focused, color, size: 24 });
+                  }
+
+                  return null;
+                }}
+                getLabelText={({ route }) => {
+                  const { options } = descriptors[route.key];
+                  const label =
+                    options.tabBarLabel !== undefined
+                      ? options.tabBarLabel
+                      : options.title !== undefined
+                        ? options.title
+                        : route.title;
+
+                  return label;
+                }}
+              />
+            )}
+          >
+            <Tab.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{
+                tabBarLabel: 'Home',
+                tabBarIcon: ({ color, size }) => {
+                  return <Icon name="home" size={size} color={color} />;
+                },
+              }}
+            />
+            <Tab.Screen
+              name="Settings"
+              component={SettingsScreen}
+              options={{
+                tabBarLabel: 'Settings',
+                tabBarIcon: ({ color, size }) => {
+                  return <Icon name="cog" size={size} color={color} />;
+                },
+              }}
+            />
+          </Tab.Navigator>
+        </Surface>
+      </PaperProvider>
+    </NavigationContainer>
+  );
+}
+
+function HomeScreen() {
+  return (
     <View style={styles.container}>
-      <SystemBars style="auto" />
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text variant="headlineMedium">Home!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text variant="headlineMedium">Settings!</Text>
     </View>
   );
 }
@@ -13,8 +112,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'grey',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });

54bd2bdd05fa50552d00b1b95d44d116a06bdd78
SanjayDevTech <sureshsanjay805@gmail.com>
2024-12-19 20:11:07 +0530

    added splash screen

--- App.js
+++ App.js
@@ -5,91 +5,134 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { NavigationContainer } from '@react-navigation/native';
 import { View, StyleSheet } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import React, { useEffect, useState, useCallback } from 'react';
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
+// Set the animation options. This is optional.
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+  const onLayoutRootView = useCallback(() => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      SplashScreen.hide();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
   return (
-    <NavigationContainer>
-      <PaperProvider>
-        <Surface style={{
-          flex: 1,
-        }}>
-          <Appbar.Header elevated>
-            <Appbar.Content title="Edge To Edge Test App" />
-          </Appbar.Header>
-          <SystemBars style="auto" />
-          <Tab.Navigator
-            screenOptions={{
-              headerShown: false,
-            }}
-            initialRouteName='Home'
-            tabBar={({ navigation, state, descriptors, insets }) => (
-              <BottomNavigation.Bar
-                navigationState={state}
-                safeAreaInsets={insets}
-                onTabPress={({ route, preventDefault }) => {
-                  const event = navigation.emit({
-                    type: 'tabPress',
-                    target: route.key,
-                    canPreventDefault: true,
-                  });
-
-                  if (event.defaultPrevented) {
-                    preventDefault();
-                  } else {
-                    navigation.dispatch({
-                      ...CommonActions.navigate(route.name, route.params),
-                      target: state.key,
+    <View onLayout={onLayoutRootView} style={{ flex: 1 }}>
+      <NavigationContainer>
+        <PaperProvider>
+          <Surface style={{
+            flex: 1,
+          }}>
+            <Appbar.Header elevated>
+              <Appbar.Content title="Edge To Edge Test App" />
+            </Appbar.Header>
+            <SystemBars style="auto" />
+            <Tab.Navigator
+              screenOptions={{
+                headerShown: false,
+              }}
+              tabBar={({ navigation, state, descriptors, insets }) => (
+                <BottomNavigation.Bar
+                  navigationState={state}
+                  safeAreaInsets={insets}
+                  onTabPress={({ route, preventDefault }) => {
+                    const event = navigation.emit({
+                      type: 'tabPress',
+                      target: route.key,
+                      canPreventDefault: true,
                     });
-                  }
-                }}
-                renderIcon={({ route, focused, color }) => {
-                  const { options } = descriptors[route.key];
-                  if (options.tabBarIcon) {
-                    return options.tabBarIcon({ focused, color, size: 24 });
-                  }
 
-                  return null;
-                }}
-                getLabelText={({ route }) => {
-                  const { options } = descriptors[route.key];
-                  const label =
-                    options.tabBarLabel !== undefined
-                      ? options.tabBarLabel
-                      : options.title !== undefined
-                        ? options.title
-                        : route.title;
+                    if (event.defaultPrevented) {
+                      preventDefault();
+                    } else {
+                      navigation.dispatch({
+                        ...CommonActions.navigate(route.name, route.params),
+                        target: state.key,
+                      });
+                    }
+                  }}
+                  renderIcon={({ route, focused, color }) => {
+                    const { options } = descriptors[route.key];
+                    if (options.tabBarIcon) {
+                      return options.tabBarIcon({ focused, color, size: 24 });
+                    }
 
-                  return label;
+                    return null;
+                  }}
+                  getLabelText={({ route }) => {
+                    const { options } = descriptors[route.key];
+                    const label =
+                      options.tabBarLabel !== undefined
+                        ? options.tabBarLabel
+                        : options.title !== undefined
+                          ? options.title
+                          : route.title;
+
+                    return label;
+                  }}
+                />
+              )}
+            >
+              <Tab.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                  tabBarLabel: 'Home',
+                  tabBarIcon: ({ color, size }) => {
+                    return <Icon name="home" size={size} color={color} />;
+                  },
                 }}
               />
-            )}
-          >
-            <Tab.Screen
-              name="Home"
-              component={HomeScreen}
-              options={{
-                tabBarLabel: 'Home',
-                tabBarIcon: ({ color, size }) => {
-                  return <Icon name="home" size={size} color={color} />;
-                },
-              }}
-            />
-            <Tab.Screen
-              name="Settings"
-              component={SettingsScreen}
-              options={{
-                tabBarLabel: 'Settings',
-                tabBarIcon: ({ color, size }) => {
-                  return <Icon name="cog" size={size} color={color} />;
-                },
-              }}
-            />
-          </Tab.Navigator>
-        </Surface>
-      </PaperProvider>
-    </NavigationContainer>
+              <Tab.Screen
+                name="Settings"
+                component={SettingsScreen}
+                options={{
+                  tabBarLabel: 'Settings',
+                  tabBarIcon: ({ color, size }) => {
+                    return <Icon name="cog" size={size} color={color} />;
+                  },
+                }}
+              />
+            </Tab.Navigator>
+          </Surface>
+        </PaperProvider>
+      </NavigationContainer>
+    </View>
   );
 }
 

9cece937068a8d096f9bba85f062c3cb5ea05f76
SanjayDevTech <sureshsanjay805@gmail.com>
2024-12-19 19:25:06 +0530

    added edge to edge

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SystemBars } from 'react-native-edge-to-edge';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <SystemBars style="auto" />
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'grey',
     alignItems: 'center',
     justifyContent: 'center',
   },
